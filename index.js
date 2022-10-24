619f2f355698c8293ba8091a66fcf285651d15e1
Rohith255 <gururohith255@outlook.com>
2022-10-24 15:12:14 +0530

    Minor UI changes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let result = document.getElementById("result");
-function fun(num) {
-  result.value += num;
-}
-function calculate() {
+const fun = (num) => {
+  return (result.value += num);
+};
+const calculate = () => {
   try {
     result.value = eval(result.value);
   } catch {
     alert("Invalid :(");
   }
-}
-function Clear() {
-  result.value = " ";
-}
-function del() {
-  result.value = result.value.slice(0, -1);
-}
+};
+const Clear = () => {
+  return (result.value = " ");
+};
+const del = () => {
+  return (result.value = result.value.slice(0, -1));
+};
